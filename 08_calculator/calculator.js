22a3dd2b3c82e1cb4271db27156616ca69d0897e
tojtt <hoangto@berkeley.edu>
2023-07-03 18:42:48 -0700

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, each) => (total += each),0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, each) => (total *= each),1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	result = 1;
+  for (let i = 0; i < b; i++){
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(factor) {
+	result = 1;
+  for (let i = 1; i < (factor+1); i++){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
